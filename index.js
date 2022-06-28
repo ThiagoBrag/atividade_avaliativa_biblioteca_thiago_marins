6db2353b81a861db52fa53ce0b67e46859032172
thiago_brag <thiago_braga@estudante.sesisenai.org.br>
2022-06-27 21:49:25 -0300

    aa
    a

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
 const router = require("./router");
 
 const app = express();
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Meu home")
+})
 
 app.use("/api", router);
 
